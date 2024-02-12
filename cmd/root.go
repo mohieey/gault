dd94171ea46334f7ac6657d51fe5a64f4a14a327
mohieey <Mohhiey@gmail.com>
2024-02-12 07:33:50 +0200

    added root command

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+)
+
+var encodingKey string
+
+var rootCmd = &cobra.Command{
+	Use:   "gault",
+	Short: "A light copy of vault by hashicorp",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&encodingKey, "key", "k", "", "The ke used to encrypt the secrets file")
+}
+
+func secretsPath() string {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return filepath.Join(home, ".secrets")
+}
